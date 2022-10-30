f8e69eb7750619d80ec2747c4b2724b18d2e29be
Mairajk <mairajkhan597@gmail.com>
2022-10-30 10:09:59 -0700

    some changes

--- server.mjs
+++ server.mjs
@@ -5,15 +5,15 @@ const port = 3000
 
 console.log("Hello");
 
-app.get('/food', (req, res) => {
-    res.send('Hello World! Here is some food for you')
+app.get('/', (req, res) => {
+    res.send('Hello World! <br/> Welcome to server by Mairaj Khan')
     console.log(`${req.ip} is looking for some food`);
 })
 
-app.get('/water', (req, res) => {
-    res.send('Hello World! Here is some water for you')
-    console.log(`${req.ip} is looking for some food`);
-})
+// app.get('/water', (req, res) => {
+//     res.send('Hello World! Here is some water for you')
+//     console.log(`${req.ip} is looking for some food`);
+// })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
